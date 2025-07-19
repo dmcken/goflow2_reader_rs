dec5e8fdc503b9e7c3083b04f50af384e8dce0f1
David Mc Ken <david.mcken@gmail.com>
2025-07-19 13:51:43 -0400

    Move csv_to_string into cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,12 @@
 // Command line interface structs and functions
 
 // std
+use std::error::Error;
 use std::fmt;
 
 // External
 use clap::{Parser,ValueEnum};
+use serde::Serialize;
 
 
 #[derive(Clone, Debug, ValueEnum, PartialEq)]
@@ -47,4 +49,35 @@ pub struct Args {
     #[arg(long = "frame", default_value_t = true, action = clap::ArgAction::SetTrue)]
     #[arg(long = "no-frame", action = clap::ArgAction::SetFalse, overrides_with = "frame")]
     pub frame: bool,
+}
+
+// Helper function to serialize a single record or iterable into CSV
+pub fn csv_to_string<T: Serialize>(value: &T, print_header: &bool) -> Result<String, Box<dyn Error>> {
+    let mut wtr: csv::Writer<Vec<u8>>;
+    if *print_header {
+        wtr = csv::WriterBuilder::new()
+            .from_writer(vec![]);
+    } else {
+        wtr = csv::WriterBuilder::new()
+            .has_headers(false)
+            .terminator(csv::Terminator::Any(b' '))
+            .from_writer(vec![]);
+    }
+
+    // Attempt to serialize the value as a record or sequence of records
+    wtr.serialize(value)?;
+    wtr.flush()?;
+
+    let mut data = wtr.into_inner()?;
+
+    // Trim trailing newline after the data record.
+    if *print_header {
+        if data.ends_with(b"\r\n") {
+            data.truncate(data.len() - 2);
+        } else if data.ends_with(b"\n") {
+            data.truncate(data.len() - 1);
+        }
+    }
+
+    Ok(String::from_utf8(data)?)
 }
